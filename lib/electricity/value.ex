df5649013882fd9d2ab40548172cfcb4b2b077dd
Steven Vandevelde <icid.asset@gmail.com>
2018-03-04 21:22:51 +0100

    Rename unit to exponent

--- lib/electricity/value.ex
+++ lib/electricity/value.ex
@@ -7,7 +7,7 @@ import TypeClass
 
 defmodule Electricity.Value do
   @moduledoc """
-  A positive non-negative integer with an associated SI unit.
+  A positive non-negative integer with an associated metric prefix.
 
   For example:
   42 micro Ohms
@@ -15,7 +15,7 @@ defmodule Electricity.Value do
   ```
   %Electricity.Value{
     amount: 42,
-    unit: -6
+    exponent: -6
   }
   ```
   """
@@ -26,11 +26,12 @@ defmodule Electricity.Value do
   # ==========
 
   defdata do
-    # Raw value to be multiplied with the unit
+    # Raw value to be combined with the metric prefix (see `power` property).
     amount :: non_neg_integer() \\ 0
 
-    # SI Unit: 10 ^ x
-    unit :: integer() \\ 0
+    # The exponent used to raise the base 10.
+    # Is used to specify a metric/si prefix.
+    exponent :: integer() \\ 0
   end
 
   # Logic
@@ -40,16 +41,16 @@ defmodule Electricity.Value do
   Reduce a Value to a Float.
   """
   @spec normalize(t()) :: float()
-  def normalize(%Value{amount: amount, unit: unit}) do
-    amount * :math.pow(10, unit)
+  def normalize(%Value{amount: amount, exponent: exponent}) do
+    amount * :math.pow(10, exponent)
   end
 
   @doc """
-  Changes the unit to zero and normalizes the value.
+  Changes the exponent to zero and normalizes the value.
   """
   @spec reduce_unit_to_zero(t()) :: t()
   def reduce_unit_to_zero(value) do
-    %{value | amount: normalize(value), unit: 0}
+    %{value | amount: normalize(value), exponent: 0}
   end
 end
 
@@ -68,7 +69,7 @@ defimpl Generator, for: Value do
   def generate(_) do
     %Value{
       amount: :rand.uniform(999_999),
-      unit: :rand.uniform(10) - 5 - 1
+      exponent: :rand.uniform(10) - 5 - 1
     }
   end
 end
@@ -82,31 +83,31 @@ definst Semigroup, for: Value do
 
       iex> left = %Value{
       ...>   amount: 1,
-      ...>   unit: 2,
+      ...>   exponent: 2,
       ...> }
       iex> right = %Value{
       ...>   amount: 1,
-      ...>   unit: -3,
+      ...>   exponent: -3,
       ...> }
       ...> append(left, right)
       %Value{
         amount: 100001,
-        unit: -3,
+        exponent: -3,
       }
 
   """
   def append(left, right) do
-    new_unit =
-      left.unit
-      |> min(right.unit)
+    new_exponent =
+      left.exponent
+      |> min(right.exponent)
       |> min(0)
 
     %Value{
-      unit: new_unit,
+      exponent: new_exponent,
       amount:
         Value.normalize(left)
         |> (fn x -> x + Value.normalize(right) end).()
-        |> (fn x -> x * :math.pow(10, new_unit * -1) end).()
+        |> (fn x -> x * :math.pow(10, new_exponent * -1) end).()
         |> round()
         |> min(0)
     }
